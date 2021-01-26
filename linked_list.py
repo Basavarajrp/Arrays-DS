3e7659d22b13b41b0d943b34f4217cb19f32f621
Basavarajrp <basavarajrp4444@gmail.com>
2021-01-26 12:36:42 +0530

    added the add_first() and add_end() methods.

--- linked_list.py
+++ linked_list.py
@@ -10,6 +10,18 @@ class Linked_List:
     def __init__(self):
         self.head = None
         
+    #Insertation of the node at the begning. Time complexity(O(1))     
+    def add_first(self,node):
+        node.next = self.head
+        self.head = node
+        
+    #Insertation of the node at the end. Time complexity(O(n))
+    def add_end(self,node):
+        n1 = self.head
+        while n1.next is not None:
+            n1 = n1.next
+        n1.next = node    
+        
     #Adding __iter__ () to traverse the LinkedlList using for loop..
     def __iter__(self):
         node = self.head
@@ -38,6 +50,9 @@ ll.head = n1
 n1.next = n2
 n2.next = n3
 n3.next = n4
+
+ll.add_first(Node("0"))
+ll.add_end(Node("e"))
 
 print(ll) 
 print()  
